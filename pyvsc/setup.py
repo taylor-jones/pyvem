9c4b5db8d216a937f9a83fb2d5fa65b8ea294aab
Taylor Jones <jonest6@oregonstate.edu>
2019-11-16 14:37:02 -0600

    Updated CLI command and description

--- pyvsc/setup.py
+++ pyvsc/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='vscm',
-    description='Visual Studio Code core & extensions management via SSH tunneling',
+    name='vsc',
+    description='VS Code extension management via SSH',
     version='0.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'vscm = pyvsc.manager:main',
+            'vsc = pyvsc.manager:main',
         ]
     },
     install_requires=[
