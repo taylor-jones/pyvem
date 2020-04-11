86a34d0233ec1ffeb4111e261bd0f80c74b11868
Taylor Jones <jonest6@oregonstate.edu>
2020-04-11 16:05:45 -0500

    Add support for differing source and destination editors; Add support for VSCodium

--- pyvsc/pyvsc/manager.py
+++ pyvsc/pyvsc/manager.py
@@ -6,6 +6,7 @@ VSCode extensions with cURL over a remote network.
 from __future__ import print_function
 
 import os
+import sys
 import re
 import platform
 import logging
@@ -14,28 +15,56 @@ import configargparse
 from time import time
 from shutil import rmtree
 from getpass import getuser
-from glob import glob
 from pyvsc.tunnel import Tunnel
 
 
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(
-  level=logging.INFO, 
+  level=logging.INFO,
   format='%(asctime)s | %(module)s [%(levelname)s] - %(funcName)s:%(lineno)d - %(message)s'
 )
 
 
+class Editors:
+    """
+    The supported VSCode editor variations
+    """
+    code = 'code'
+    codium = 'codium'
+    insiders = 'code-insiders'
+
+
 class Manager():
     def __init__(self, **kwargs):
+        self.dry_run = kwargs.get('dry_run')
+        self.keep = kwargs.get('keep')
         self.extensions_dir = None
 
         # determine which version of the editor to work with, and make
         # sure that version is installed and on the PATH
-        self.insiders = kwargs.get('insiders', False)
-        self.cmd = 'code-insiders' if self.insiders else 'code'
-        self._check_editor_is_installed()
+        self.insiders = kwargs.get('insiders')
+        self.codium = kwargs.get('codium')
+        
+        # if either insiders or codium was explicitely specified, use that
+        # editor for both the source and destination editors. Otherwise,
+        # inspect both the source and destination editors individually to
+        # determine which Code editors to use.
+        if self.insiders:
+            self.cmd_source = Editors.insiders
+            self.cmd_dest = Editors.insiders
+        elif self.codium:
+            self.cmd_source = Editors.codium
+            self.cmd_dest = Editors.codium
+        else:
+            self.cmd_source = self._get_editor_command(
+                kwargs.get('source_editor'), Editors.code)
+            self.cmd_dest = self._get_editor_command(
+                kwargs.get('dest_editor'), Editors.code)
 
-        # establish the ssh tunnel
+        # ensure the source & destination editors are installed on the system
+        self._check_editors_are_installed([self.cmd_source, self.cmd_dest])
+
+        # establish the ssh tunnel connection
         self.tunnel = Tunnel(
             host=kwargs.get('ssh_host'),
             port=kwargs.get('ssh_port'),
@@ -43,31 +72,74 @@ class Manager():
         )
 
         # determine the output directory and specified extensions
-        self.keep = kwargs.get('keep')
         self.output = self._process_output_directory(kwargs.get('output_dir'))
         self.extensions = self._process_extensions(kwargs.get('extensions'))
 
 
-    def _check_editor_is_installed(self):
+    def _get_editor_command(self, command, default=None):
+        """
+        Parse a string to determine if a specific version of VS Code is present
+
+        Arguments:
+            command {str} -- the content to inspect
+
+        Returns:
+            {str|None} -- the editor command associated with the input,
+                if found. Otherwise, the default value.
+        """
+        command = command.lower()
+        
+        if command in ['code-insiders', 'insiders', 'vscode-insiders']:
+            return Editors.insiders
+        elif command in ['codium', 'vscodium', 'vs-codium']:
+            return Editors.codium
+        elif command in ['code', 'vscode', 'vs-code']:
+            return Editors.code
+        else:
+            return default
+
+
+    def _check_editors_are_installed(self, editors):
         """
         Checks if the specified version of VS Code is installed.
         Raises an error if not.
-        
+
+        Arguments:
+            editors {list} -- a list of {1,2} Code editors to validate the
+                installation of.
+
         Returns:
             bool -- True, unless an exception is raised.
         """
-        try:
-            self.version = os.popen('%s --version' % (
-              self.cmd)).read().splitlines()[0]
-            return True
-        except RuntimeError as e:
-            LOGGER.error('The command "%s" is not on your path. Please ' \
-              'make sure the correct version of VS Code is installed before ' \
-              'running this program.' % (self.cmd))
-            exit(1)
+        for editor in editors:
+            try:
+                self.version = os.popen('%s --version' % (
+                editor)).read().splitlines()[0]
+                return True
+            except RuntimeError as e:
+                LOGGER.error('The command "%s" is not on your path. Please ' \
+                'make sure the correct version of VS Code is installed ' \
+                'before running this program.' % (editor))
+                sys.exit(1)
 
 
     def _get_valid_dir(self, directory, create_if_not_exists=False):
+        """
+        Attempts to validate a relative or absolute directory path. If a valid
+        path is determined, the valid path is returned. If no valid directory
+        path is able to be determined, None is returned.
+
+        Arguments:
+            directory {str} -- The path to a directory on the file system.
+
+        Keyword Arguments:
+            create_if_not_exists {bool} -- If True (and the directory does not
+                yet exist) a directory will be created at the specified path
+                (default: {False})
+
+        Returns:
+            {str|None}
+        """
         d = os.path.expanduser(directory)
         d = os.path.abspath(d)
         dir_exists = os.path.isdir(d)
@@ -88,7 +160,7 @@ class Manager():
         except Exception as e:
           LOGGER.error('Could not validate directory: %s' % (d))
           LOGGER.exception(e)
-          exit(1)
+          sys.exit(1)
 
 
     def _get_directory_vsix_files(self, directory):
@@ -119,8 +191,8 @@ class Manager():
             return '%s/darwin/%s' % (url_base, version)
         else:
             LOGGER.error('Sorry, pyvsc doesn\'t currently support %s' % (
-              operating_system))
-            exit(1)
+                operating_system))
+            sys.exit(1)
 
 
     def _get_vscode_curl_command(self, url):
@@ -145,6 +217,12 @@ class Manager():
         name of the extension in the format of {publisher}.{package}
 
         ex: ms-python.python
+        
+        Arguments:
+            extension {str} -- the name of the extension
+        
+        Returns:
+            {str}
         """
         publisher, package = extension.split('.')
         return 'https://%s.gallery.vsassets.io/_apis/public/gallery' \
@@ -193,16 +271,19 @@ class Manager():
 
 
     def download(self):
+        """
+        Performs a "download" operation for the specified extensions.
+        """
         if self.extensions == None:
             LOGGER.error('No extensions have been specified.')
-            exit(1)
+            sys.exit(1)
 
         if self.output == None:
             LOGGER.error('No output directory has been specified.')
-            exit(1)
+            sys.exit(1)
 
         LOGGER.info('Downloading %d extensions to %s' % (
-          len(self.extensions), self.output))
+            len(self.extensions), self.output))
 
         # download each extension to the output directory in the ssh tunnel.
         for ext in self.extensions:
@@ -231,7 +312,7 @@ class Manager():
         Installs an individual VSIX extensions at a specified path.
         """
         LOGGER.info('Installing %s' % (os.path.basename(path)))
-        os.system('%s --install-extension %s' % (self.cmd, path))
+        os.system('%s --install-extension %s' % (self.cmd_dest, path))
 
 
     def install(self, extension_path=None):
@@ -250,7 +331,7 @@ class Manager():
                 self._install_extension('%s/%s' % (extension_path, f))
         else:
             LOGGER.error('Cannot install extension(s) from the path "%s".' % (
-              extension_path))
+                extension_path))
 
 
     def update(self):
@@ -290,8 +371,9 @@ class Manager():
         # if the user did not specify any extensions, we'll assume that they
         # want to update all of their currently-installed extensions.
         if extensions is None or extensions == '*':
-            LOGGER.debug('No extensions specified. Finding all extensions')
-            extensions = os.popen('code --list-extensions').read().splitlines()
+            LOGGER.debug('Processing extensions from %s.' % (self.cmd_source))
+            extensions = os.popen('%s --list-extensions' % (self.cmd_source)
+            ).read().splitlines()
 
         # otherwise, if we were given a string of one or more extensions,
         # we need to determine if we were given a path to a directory of
@@ -306,7 +388,7 @@ class Manager():
                 self.extensions_dir = directory
                 extensions = self._get_directory_vsix_files(directory)
 
-            # If a literal string of extension names was provided, convert the 
+            # If a literal string of extension names was provided, convert the
             # string to a list of extensions.
             else:
                 extensions = re.split(';|,', extensions)
@@ -316,7 +398,7 @@ class Manager():
             extensions = list(extensions)
         except TypeError as e:
             LOGGER.error('Could not identify a list of extensions.')
-            exit(1)
+            sys.exit(1)
 
         return extensions
 
@@ -343,6 +425,7 @@ class CustomFormatter(configargparse.HelpFormatter):
                 for option_string in action.option_strings:
                     parts.append('%s' % (option_string))
                 parts[-1] += ' %s'%args_string
+
             return ', '.join(parts)
 
 
@@ -357,8 +440,12 @@ def main():
     parser.add('-o', '--output-dir', default='/tmp/vsc-%d' % (time() * 1000), help='The directory where the extensions will be downloaded.')
     parser.add('-e', '--extensions', default='*', help='A string, list, or directory of extensions to download/update/install')
     parser.add('-k', '--keep', default=False, action='store_true', help='If set, downloaded .vsix files will not be deleted')
-    parser.add('-i', '--insiders', default=False, action='store_true', help='Install extensions to VS Code Insiders instead of VS Code')
+    parser.add('-s', '--source-editor', default='', help='The editor that will be used to identify extensions')
+    parser.add('-d', '--dest-editor', default='', help='The editor where the extensions will be installed')
     parser.add('-v', '--verbose', default=False, action='store_true', help='Display more program output')
+    parser.add('-n', '--dry-run', default=False, action='store_true', help='Preview the action(s) that would be taken without actually taking them')
+    parser.add('--insiders', default=False, action='store_true', help='Use VSCode Insiders as the source and destination editor')
+    parser.add('--codium', default=False, action='store_true', help='Use VSCodium as the source and destination editor')
     parser.add('--ssh-host', default='localhost', help='SSH Host IP or network name')
     parser.add('--ssh-port', default=22, help='SSH Port')
     parser.add('--ssh-user', default=getuser(), help='SSH username')
@@ -369,29 +456,48 @@ def main():
     # if verbose mode was requested, update the logging level
     if options.verbose:
         LOGGER.setLevel(__debug__)
+        print('vsc invoked with the following options:')
+        for key, value in vars(options).items():
+            print('%12s: %s' % (key, value))
+        print('')
 
     # if no actionable command was provided, just print
     # the help output, but don't execute anything else.
     if not options.command:
         print(parser.format_help())
-        return
+        sys.exit(1)
 
-    # print startup config
-    LOGGER.debug('vsc started with the following options:')
-    for key,value in vars(options).items():
-        LOGGER.debug('%12s: %s' % (key, value))
+    # validate the options
+    if options.insiders and options.codium:
+        LOGGER.error(
+            'You\'ve specified to use both VSCode Insiders and VS Codium. '
+            'Only one of these options is allowed at a time, since using '
+            'either of them overrides both the source and destination '
+            'editors. If you need to specify different code editors for '
+            'the source and destination, you can do so using:\n'
+            '-s, --source\t\t-d, --dest')
+        sys.exit(1)
 
     # initialize an instance of the VSC Manager
     cmd = options.command
     manager = Manager(
         extensions=options.extensions, 
         output_dir=options.output_dir,
+        source_editor=options.source_editor,
+        dest_editor=options.dest_editor,
+        dry_run=options.dry_run,
         ssh_host=options.ssh_host,
         ssh_port=options.ssh_port,
         ssh_user=options.ssh_user,
         keep=cmd=='download' or cmd=='install' or options.keep,
         insiders=options.insiders,
+        codium=options.codium,
     )
+
+    # if it's just a dry-run, don't perform the action
+    if options.dry_run:
+        LOGGER.info('\nDry-run only. Exiting..')
+        sys.exit(0)
 
     # perform the desired operation
     if cmd == 'download':
