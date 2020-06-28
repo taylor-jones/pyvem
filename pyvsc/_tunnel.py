c3f4225a395c98504476a71fa5cb11894b6e2df2
Taylor Jones <jonest6@oregonstate.edu>
2020-06-28 13:07:15 -0500

    Update tunnel for better python3 support, improved logging

--- pyvsc/_tunnel.py
+++ pyvsc/_tunnel.py
@@ -1,44 +1,46 @@
 """The Tunnel provides a means of communicating with a remote host."""
 
-from __future__ import print_function, absolute_import
-
 import sys
 from socket import gethostname
 from getpass import getpass
+
 from fabric import Connection
 from rich.console import Console
 
 from pyvsc._config import rich_theme
-from pyvsc._containers import ConnectionParts, AttributeDict
+from pyvsc._containers import ConnectionParts
 from pyvsc._logging import get_rich_logger
 
-_console = Console(theme=rich_theme)
 
-class Tunnel:
+_console = Console(theme=rich_theme)
+_LOGGER = get_rich_logger(__name__, console=_console)
+
+
+class Tunnel():
     """
     Communicate with the remote host.
 
-    The tunnel class establishes a connection to the remote system and allows
-    a medium for getting remote resources and transferring them to the local
-    system.
+    The tunnel class establishes a connection to the remote system and allows a medium for
+    getting remote resources and transferring them to the local system.
     """
-    logger = get_rich_logger(__name__, console=_console)
-
 
     def __init__(self, ssh_host=None, ssh_gateway=None, autoconnect=False):
         """
         Apply argued ssh connection arguments. Setup connection if specified.
 
         Keyword Arguments:
-            ssh_host {ConnectionParts} -- the ssh_host connection parts
-                (default: {None})
-            ssh_gateway {ConnectionParts} -- the ssh_gateway connection parts
-                (default: {None})
-            autoconnect {bool} -- If True, go ahead and try to connect to
-                the remote host at initialization time (default: {False})
+            ssh_host {ConnectionParts} -- the ssh_host connection parts (default: {None})
+            ssh_gateway {ConnectionParts} -- the ssh_gateway connection parts (default: {None})
+            autoconnect {bool} -- If True, go ahead and try to connect to the remote host at
+                initialization time (default: {False})
         """
-        self.ssh_host = None
-        self.ssh_gateway = None
+        self._connection = None
+        self._localhost_name = gethostname()
+        self._ssh_host = None
+        self._ssh_gateway = None
+
+        # expose the module logger so the loglevel can be updated by external callers.
+        self.logger = _LOGGER
 
         # maintain a list of all the directories we create during processing
         # so we can go back and clean them up before exiting.
@@ -50,31 +52,34 @@ class Tunnel:
 
 
     def apply(self, ssh_host, ssh_gateway):
+        # TODO: Better handle cases where we don't want to adopt the password from the
+        # ssh host connection parameters.
         """
         Apply the ssh_host and ssh_gateway values to the current tunnel.
 
-        The SSH gateway (if one exists) will adopt most any value from the
-        SSH host specification, except for the hostname.
+        The SSH gateway (if one exists) will adopt most any value from the SSH host
+            specification, except for the hostname.
 
         Arguments:
             ssh_host {ConnectionParts}
             ssh_gateway {ConnectionParts}
         """
-        self.ssh_host = ssh_host
+        self._ssh_host = ssh_host
         if ssh_gateway:
-            self.ssh_gateway = ConnectionParts(
+            self._ssh_gateway = ConnectionParts(
                 hostname=ssh_gateway.hostname,
                 username=ssh_gateway.username or ssh_host.username,
                 password=ssh_gateway.password or ssh_host.password,
-                port=ssh_gateway.port or ssh_host.port)
+                port=ssh_gateway.port or ssh_host.port,
+            )
 
 
     def ensure_connection(self):
         """
         Check for existing ssh connection.
 
-        Wrapper of connect() which just assumes checking for an existing
-        connection based on pre-existing ssh configurations.
+        Wrapper of connect() which just assumes checking for an existing connection based on
+        pre-existing ssh configurations.
         """
         self.connect()
 
@@ -86,13 +91,12 @@ class Tunnel:
         Keyword Arguments:
             ssh_host {ConnectionParts} -- (default: {None})
             ssh_gateway {ConnectionParts} -- (default: {None})
-            force {bool} -- If True, the tunnel will attempt to connect
-                regardless of whther or not its connected. (default: {False})
+            force {bool} -- If True, the tunnel will attempt to re-establish a connection,
+                ignoring whether or not a connection already exists. (default: {False})
         """
         if force or not self.is_connected():
-            self._connection = self.get_connection(
-                ssh_host=ssh_host or self.ssh_host,
-                ssh_gateway=ssh_gateway or self.ssh_gateway)
+            self._connection = self.get_connection(ssh_host=ssh_host or self._ssh_host,
+                                                   ssh_gateway=ssh_gateway or self._ssh_gateway)
 
 
     def is_connected(self):
@@ -102,10 +106,7 @@ class Tunnel:
         Returns:
             bool -- True if connected, False if not.
         """
-        try:
-            return self._connection.is_connected
-        except Exception as e:
-            return False
+        return self._connection and self._connection.is_connected
 
 
     def get_connection(self, ssh_host, ssh_gateway=None):
@@ -117,19 +118,17 @@ class Tunnel:
             ssh_gateway {ConnectionParts}
 
         Returns:
-            Connection -- an instance of a fabric Connection object
-                that represents a valid ssh connection to the remote host.
+            Connection -- an instance of a fabric Connection object that represents a valid ssh
+            connection to the remote host.
 
         Note:
-            The current implementation assumes that the username, port,
-            and password are the same for the ssh host and the gateway.
+            The current implementation assumes that the username, port, and password are the
+            same for the ssh host and the gateway.
         """
         password = ssh_host.password
 
         if not password:
-            prompt = '{}@{} password: '.format(
-                ssh_host.username,
-                ssh_host.hostname)
+            prompt = '{}@{} password: '.format(ssh_host.username, ssh_host.hostname)
 
             # get a password from the user
             try:
@@ -138,12 +137,15 @@ class Tunnel:
                 else:
                     print(prompt)
                     password = sys.stdin.readline().rstrip()
-                self.ssh_host.password = password
+                self._ssh_host.password = password
 
             except KeyboardInterrupt:
-                Tunnel.logger.warning('Exiting.')
+                _LOGGER.warning('Exiting.')
                 sys.exit(1)
 
+        # TODO: Abstract the gateway connection to a more generalized "connection" method so we
+        # can more specifically pinpoint (in the event of a connection error) whether the error
+        # occurred while trying to connect to the host or the gateway.
         try:
             # establish the ssh connection
             connection = Connection(
@@ -161,13 +163,13 @@ class Tunnel:
 
             # open the ssh connection to test the connection
             connection.open()
-            Tunnel.logger.info('Connected to host: {}.'.format(
-                ssh_host.hostname))
+            _LOGGER.info('Connected to host: %s.', ssh_host.hostname)
             return connection
 
-        except Exception as e:
-            raise ConnectionError('Failed to connect to host: {}'.format(
-                ssh_host.hostname))
+        # FIXME: Allow for authentication error a couple times before exiting due to wrong password
+        except Exception as err:
+            _LOGGER.error('Failed to connect to host "%s". %s', ssh_host.hostname, err)
+            sys.exit(1)
 
 
     def get(self, remote, local):
@@ -180,8 +182,8 @@ class Tunnel:
         """
         self.ensure_connection()
         self._connection.get(remote=remote, local=local)
-        Tunnel.logger.debug('Copied "{}:{}" to "{}:{}"'.format(
-            self.ssh_host.hostname, remote, gethostname(), local))
+        _LOGGER.debug('Copied "%s:%s" to "%s:%s"', self._ssh_host.hostname, remote,
+                      self._localhost_name, local)
 
 
     def rmdir(self, path):
@@ -190,46 +192,62 @@ class Tunnel:
 
         Arguments:
             path {str} -- the path to the file or directory to remove
+
+        Returns:
+            bool -- True if the remote directory was able to be successfully removed, False if not
         """
         self.ensure_connection()
         res = self._connection.run('rm -rf {}'.format(path))
 
         if res.exited == 0:
-            Tunnel.logger.debug('Removed remote directory: "{}:{}"'.format(
-                self.ssh_host.hostname, path))
+            _LOGGER.debug('Removed remote directory: "%s:%s"', self._ssh_host.hostname, path)
+            self.created_dirs.remove(path)
             return True
+
         return False
 
 
     def mkdir(self, path):
         """
-        Create a directory on the remote system.
+        Create a directory on the remote system and adds the directory to the running set of
+        created directories, so we can go back and delete all the created directories at the end
+        of processing.
 
         Arguments:
             path {str} -- the path to the directory to create
+
+        Returns:
+            bool -- True if the remove directory was able to be created, False if not
         """
         self.ensure_connection()
-        res = self._connection.run('mkdir -p {}'.format(path))
-        self.created_dirs.add(path)
-        return res.exited == 0
+        res = self._connection.run('mkdir -p %s' % path)
+
+        if res.exited == 0:
+            _LOGGER.debug('Created remote directory: "%s:%s"', self._ssh_host.hostname, path)
+            self.created_dirs.add(path)
+            return True
+
+        return False
 
 
     def cleanup_created_dirs(self):
-        """Cleanup directories that were created during processing."""
+        """
+        Cleanup directories that were created during processing (if any exist).
+        """
         if not bool(self.created_dirs):
-            Tunnel.logger.debug('No remote directories need to be cleaned up.')
-            return
+            _LOGGER.debug('No remote directories need to be cleaned up.')
 
-        if not self.is_connected():
-            dirs = ', '.join(list(self.created_dirs))
-            Tunnel.logger.error('Tunnel connection was lost. Unable to remove '
-                          'remote directories: {}'.format(dirs))
-
-        for d in self.created_dirs:
-            try:
-                self.rmdir(d)
-            except Exception as e:
-                Tunnel.logger.error(e)
+        elif not self.is_connected():
+            _LOGGER.error('Tunnel connection was lost. Unable to remove remote directories: %s. '
+                          '\nYou may need to manually remove them.',
+                          ', '.join(list(self.created_dirs)))
+        else:
+            # There are directories that need to be removed and the remote connection is active
+            for directory in self.created_dirs:
+                try:
+                    self.rmdir(directory)
+                except EnvironmentError as err:
+                    _LOGGER.error(err)
 
 
     def run(self, command, hide=True):
@@ -249,13 +267,14 @@ class Tunnel:
 
     def close(self):
         """Close the remote SSH connection."""
-        try:
+        if self._connection is not None:
             self._connection.close()
-            Tunnel.logger.debug('Closed ssh tunnel connection.')
-        except Exception as e:
-            pass
+            _LOGGER.debug('Closed ssh tunnel connection.')
 
 
     def __del__(self):
         """Close connection in class destructor."""
-        self.close()
+        try:
+            self.close()
+        except ImportError:
+            pass
