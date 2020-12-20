5c2c593aa5a8ee1e6d96d57780373b2b487f840b
Taylor Jones <jonest6@oregonstate.edu>
2020-12-20 12:34:52 -0600

    Add pylint to setup.cfg; Remove usage: vem <command> [options]
    where <command> is one of:
    	config, help, info, install, list, outdated, search, update, version
    
    For help about a specific command:
    	vem help <command>
    
    VSCode CLI helper for editors and extensions Args that start with '--' (eg.
    -h) can also be set in a config file (.vemrc or ~/.vemrc or ~/.config/.vemrc).
    Config file syntax allows: key=value, flag=true, stuff=[a,b,c] (for details,
    see syntax at https://goo.gl/R74nmi). If an arg is specified in more than one
    place, then commandline values override config file values which override
    defaults.
    
    positional arguments:
      command               The main vem command to execute.
      args                  The command arguments.
    
    required named arguments:
      -h, --ssh-host SSH_HOST
                            Specify a SSH host in the form [user@]server[:port].
      -g, --ssh-gateway SSH_GATEWAY
                            Specify a SSH gateway in the form
                            [user@]server[:port].
    
    optional named arguments:
      -o, --output-dir OUTPUT_DIR
                            The directory where the extensions will be downloaded.
    
    optional arguments:
      --help                Show help and exit.
      -V, --version         Show version and exit.
      --no-cleanup          Do not remove temporary downloads on the local
                            machine.
      -v, --verbose         Show debug output.
      -q, --quiet           Show only the minimally necessary output.
      --code                (default) Use VSCode as the target editor.
      --insiders            Use VSCode Insiders as the target editor.
      --exploration         Use VSCode Exploration as the target editor.
      --codium              Use VSCodium as the target editor. console script, bump python requirement to 3.6+, update required packages

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.3-dev'
+__version__ = '0.4-dev'
 
 setup(
     name='pyvsc',
-    description='VS Code extension management over SSH',
+    description='VSCode extension management over SSH',
     version=__version__,
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'vsc = pyvsc.manager:main',
-            'vem = pyvsc.main:main',
+            'vem = pyvsc.main:main'
         ]
     },
     install_requires=[
         'configargparse',
         'coloredlogs',
         'fabric',
+        'fuzzywuzzy',
         'paramiko',
+        'python-Levenshtein',
+        'rich',
         'semantic_version',
     ],
-    python_requires='>=2.7'
+    python_requires='>=3.6'
 )
