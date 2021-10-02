fbb4344bf956ffb00146a912f1a77ab3548cd385
Taylor Jones <jonest6@oregonstate.edu>
2021-10-02 17:05:37 -0500

    Clean help generator; start adding strong typing for commands

--- pyvem/_command.py
+++ pyvem/_command.py
@@ -1,16 +1,18 @@
 """Abstract Command class from which all program command subclasses inheir"""
 
+import logging
 import os
 import pathlib
 
-from rich.console import Console
+from typing import List, Dict
+from configargparse import ArgumentParser, Namespace
 
+from rich.console import Console
 from pyvem._tunnel import Tunnel
 from pyvem._marketplace import Marketplace
 from pyvem._help import Help
 from pyvem._config import rich_theme
 from pyvem._logging import get_rich_logger
-
 
 _console = Console(theme=rich_theme)
 _LOGGER = get_rich_logger(__name__, console=_console)
@@ -20,44 +22,45 @@ class Command():
     """
     Abstract base command class from which all actionable commands inherit.
     """
-    tunnel = Tunnel()
-    marketplace = None
-    main_parser = None
-    main_options = None
+    tunnel: Tunnel = Tunnel()
+    marketplace: Marketplace = None
+    main_parser: ArgumentParser = None
+    main_options: Namespace = None
 
     # Keep track of temporary files created during execution.
     # These will be removed at the end of processing.
-    temporary_file_paths = []
+    temporary_file_paths: List[str] = []
 
 
-    def __init__(self, name, help_, aliases=None):
-        self.name = name
-        self.help = help_
-        self.aliases = aliases or []
+    def __init__(self, name: str, help: Help, aliases: List[str] = None):
+        self.name: str = name
+        self.help: Help = help
+        self.aliases: List[str] = aliases or []
 
-        # Keep track of whether this program created the local output directory.
-        # If we created it, we'll delete it. If we didn't create it, we won't delete it.
-        self.created_local_output_dir = None
+        # Keep track of whether this program created the local output dir.
+        # If we created it, we'll delete it. If we didn't create it, we won't
+        # delete it.
+        self.created_local_output_dir: str = None
 
-        # Ensure all sub-commands have instantiated a Help instance for their 'help' attribute.
+        # Ensure all sub-commands have instantiated a Help instance for their
+        # 'help' attribute.
         assert isinstance(self.help, Help)
 
-
     @staticmethod
-    def store_temporary_file_path(path):
+    def store_temporary_file_path(path: str) -> None:
         """
-        Adds a given file path to the list of temporary files that should be removed at the
-        end of processing.
+        Adds a given file path to the list of temporary files that should be
+        removed at the end of processing.
 
         Arguments:
-            path {str} -- The absolute path to a file.
+            path -- The absolute path to a file.
         """
         if path:
             Command.temporary_file_paths.append(path)
 
 
     @staticmethod
-    def remove_temporary_files():
+    def remove_temporary_files() -> None:
         """
         Remove all temporary files that were created during processing.
         """
@@ -67,30 +70,32 @@ class Command():
 
 
     @staticmethod
-    def apply_log_level(logger):
+    def apply_log_level(logger: logging.Logger) -> None:
         """
         Update the logger to apply the log-level from the main options.
-        NOTE: This isn't applied in the base Command class, but it's used by Command subclasses.
+        NOTE: This isn't applied in the base Command class, but it's used by
+        Command subclasses.
 
         Arguments:
-            logger {logging.logger} -- The logger from a command.
+            logger -- The logger from a command.
         """
         logger.setLevel(Command.main_options.log_level)
 
 
     @staticmethod
-    def show_error(text, **kwargs):
+    def show_error(text: str, **kwargs) -> None:
         """
-        Print a styled error using the rich_theme error styling and a rich console. This is
-        suitable for text that is intended to all use the error style. For error messages that
-        need for fine-grained output, it's probably better to write the custom message message
+        Print a styled error using the rich_theme error styling and a rich
+        console. This is suitable for text that is intended to all use the
+        error style. For error messages that need for fine-grained output,
+        it's probably better to write the custom message message
 
-        NOTE: The difference between this and the logging.error is that the console error does
-        not include the logging formatting (like the module name and timestamp). This is just
-        for direct error messages.
+        NOTE: The difference between this and the logging.error is that the
+        console error does not include the logging formatting (like the module
+        name and timestamp). This is just for direct error messages.
 
         Arguments:
-            text {str} -- The error message to output
+            text -- The error message to output
 
         NOTE: **kwargs are passed to the console.print() method, so any named
         arguments that console.print() supports are supported here as well.
@@ -99,19 +104,21 @@ class Command():
         _console.print(text, style=rich_theme.styles['error'], **kwargs)
 
 
-    def ensure_output_dirs_exist(self):
+    def ensure_output_dirs_exist(self) -> bool:
         """
-        Ensure that both the temprorary remote directory and the temporary local directory exist.
+        Ensure that both the temprorary remote directory and the temporary
+        local directory exist.
 
         Returns:
-            bool -- True if we could ensure both the remote and local dirs exist, False if not.
+            bool -- True if we could ensure both the remote and local dirs
+            exist, False if not.
         """
         local_exists = self.ensure_local_output_dir_exists()
         remote_exists = self.ensure_remote_output_dir_exists()
         return local_exists and remote_exists
 
 
-    def ensure_remote_output_dir_exists(self):
+    def ensure_remote_output_dir_exists(self) -> bool:
         """
         Ensure the temporary remote directory that will be used for storing
         any downloads on the remote system exists.
@@ -123,7 +130,7 @@ class Command():
         return self.tunnel.mkdir(self.main_options.remote_output_dir)
 
 
-    def ensure_local_output_dir_exists(self):
+    def ensure_local_output_dir_exists(self) -> bool:
         """
         Create the output directory provided by the original vem command
 
@@ -140,7 +147,8 @@ class Command():
             return True
 
         try:
-            pathlib.Path(Command.main_options.output_dir).mkdir(parents=True, exist_ok=False)
+            pathlib.Path(Command.main_options.output_dir).mkdir(
+                parents=True, exist_ok=False)
             self.created_local_output_dir = True
             return True
         except EnvironmentError as err:
@@ -148,15 +156,15 @@ class Command():
             return False
 
 
-    def invoke(self, main_parser, main_options):
+    def invoke(self, main_parser: ArgumentParser, main_options: Namespace) -> None:
         """
         Prepare a Command object's static elements in order to make them
         available to all Command subclasses. Then invokes the run() command
         on the object to run the vem command that was actually called.
 
         Arguments:
-            main_parser {configargparse.ArgParser} -- The ArgParser from the top-level module.
-            main_options {Namespace} -- The namespace object containing all of the argument options
+            main_parser -- The ArgParser from the top-level module.
+            main_options -- The Namespace containing all of the arg options
         """
         if not Command.main_options and not Command.main_parser:
             # store the main parser and the the main options so they can
@@ -167,8 +175,9 @@ class Command():
             # apply the log level from the original parsed arguments
             _LOGGER.setLevel(Command.main_options.log_level)
 
-            # pass along the original remote connection args to the shared tunnel instance, so it
-            # can be ready to connect whenever a command that uses the remote connection is invoked
+            # pass along the original remote connection args to the shared
+            # tunnel instance, so it can be ready to connect whenever a command
+            # that uses the remote connection is invoked
             Command.tunnel.logger.setLevel(main_options.log_level)
             Command.tunnel.apply(ssh_host=main_options.ssh_host,
                                  ssh_gateway=main_options.ssh_gateway)
@@ -177,45 +186,34 @@ class Command():
             # so we don't have to maintain multiple tunnel connections.
             Command.marketplace = Marketplace(tunnel=Command.tunnel)
 
-        # Invoke the run() method on the called command. The run() method must be implemented
-        # within each command subclass.
+        # Invoke the run() method on the called command. The run() method must
+        # be implemented within each command subclass.
         try:
-            # TODO: This should be better handled. Right now, it would print the message
-            # below if the Command.run() returns True, but I'm not sure how much value that adds.
+            # TODO: This should be better handled. Right now, it would print
+            # the message below if the Command.run() returns True, but I'm not
+            # sure how much value that adds.
             if self.run():
                 _LOGGER.info('All Done!')
-            # else:
-            #     _console.print('[logging.level.warning]Command finished[/]')
-
         except Exception as err:
             _LOGGER.exception(repr(err))
-            # raise err
-
         finally:
-            # regardless of what happens, cleanup and created remote
-            # directories and close the remote connection.
+            # Regardless of what happens, cleanup any created remote dirs and
+            # close the remote connection.
             self.tunnel.cleanup_created_dirs()
             self.tunnel.close()
             self.remove_temporary_files()
 
 
-    def show_help(self):
+    def show_help(self) -> None:
         """
         Invoke the print() method on a Command's help object instance.
-        NOTE: This expects that each Command sub-class implements a Help class instance
+        NOTE: This expects that each Command sub-class implements a Help class
+        instance
         """
         self.help.print_help()
 
 
-    def show_usage(self):
-        """
-        Prints the synopsis of a command's help info.
-        NOTE: This expects that each Command sub-class implements a Help class instance
-        """
-        self.help.print_usage()
-
-
-    def get_command_parser(self, *args, **kwargs):
+    def get_command_parser(self, *args, **kwargs) -> ArgumentParser:
         """
         Implements parsing subcommands within a particular command.
         NOTE: This needs to be implemented in each of the sub-classes
@@ -223,10 +221,12 @@ class Command():
         Raises:
             NotImplementedError: [description]
         """
+        # This exception isn't raised if the Command subclass has implemented
+        # this method.
         raise NotImplementedError
 
 
-    def run(self, *args, **kwargs):
+    def run(self, *args, **kwargs) -> None:
         """
         Implements the funcionality of a particular command.
         NOTE: This needs to be implemented in each of the sub-classes
@@ -234,4 +234,6 @@ class Command():
         Raises:
             NotImplementedError: [description]
         """
+        # This exception isn't raised if the Command subclass has implemented
+        # this method.
         raise NotImplementedError
--- pyvem/_help.py
+++ pyvem/_help.py
@@ -1,67 +1,55 @@
-"""Help class used within Command instances to organize the components of a Help output message"""
+"""Help class used within Command instances to organize the components of a
+Help output message
+"""
 
 import re
 from textwrap import dedent
+from typing import Tuple
 
 from rich.console import Console
-from rich.text import Text
+from rich.text import Text, Lines
 
 from pyvem._util import shell_dimensions
 from pyvem._config import rich_theme
+from rich.theme import Theme
 
 _DEFAULT_WRAP_WIDTH = 80
-_DEFAULT_INDENT = '    '
 _DEFAULT_PAD_SIZE = 4
 
 _text = Text(tab_size=4)
 _console = Console(theme=rich_theme)
 
 
-def _rich_wrapped(text, style):
+def _rich_themed(text: str, style: str, theme: Theme = rich_theme) -> str:
     """
-    Prepares a string for rich formatting by wrapping it in rich-formatted syntax.
+    Wraps a string in rich-formatted syntax.
 
     Arguments:
-        text {str} -- The text to return as rich-formatted
-        style {str} -- A rich formatting style (space-separated)
-            NOTE: This should be original rich-valid statements like:
-            'red bold', etc., not custom theme styles.
-
-    Returns:
-        str -- The wrapped string, ready to pass to a rich Console for output.
-    """
-    return '[{0}]{1}[/{0}]'.format(style, text)
-
-
-def _rich_themed(text, style, theme=rich_theme):
-    """
-    Prepares a string for rich formatting by wrapping it in rich-formatted syntax.
-
-    Arguments:
-        text {str} -- The text to return as rich-formatted
-        style {str} -- The name of an existing style from the rich theme.
-        theme {rich.Theme} -- The name of an existing rich theme.
+        text -- The text to return as rich-formatted
+        style -- The name of an existing style from the rich theme.
+        theme -- The name of an existing rich.Theme.
             NOTE: This should be custom styles belonging to the theme
+
     Returns:
-        str -- The wrapped string, ready to pass to a rich Console for output.
+        The wrapped string, ready to pass to a rich Console for output.
     """
     return '[{0}]{1}[/{0}]'.format(theme.styles[style], text)
 
 
-def _rich_command_heading(text):
+def _rich_command_heading(text: str) -> str:
     """
     Returns a rich-formatted string using a pre-defined heading style.
 
     Arguments:
-        text {str} -- The string to wrap in a rich heading
+        text -- The string to wrap in a rich heading
 
     Returns:
-        str -- The heading-wrapped text.
+        The heading-wrapped text.
     """
     return _rich_themed(text, 'h1')
 
 
-def _current_wrap_width():
+def _current_wrap_width() -> int:
     """
     Helps determine the current width at which console text should
     wrap by finding the min of the current width and the default wrap width.
@@ -73,48 +61,31 @@ def _current_wrap_width():
     return min(_column_width, _DEFAULT_WRAP_WIDTH)
 
 
-def _wrap(content, pad_size=_DEFAULT_PAD_SIZE):
+def _wrap_for_console(content: str) -> Lines:
     """
-    Wraps a string such that it fits within the current console width, while padding each
-    line of the wrapped text with a specified amount of padding.
+    Wraps a string such that it fits within the current console width, while
+    padding each line of the wrapped text with a specified amount of padding.
 
     Arguments:
-        content {str} -- a string to wrap
-        pad_size {int} -- the number of spaces to pad the content.
+        content -- a string to wrap
+        padsize -- the number of spaces to pad the content.
 
     Returns:
-        Text -- a rich text, ready to be printed with a rich Console.
+        A rich.Text instance, ready to be printed with a rich.Console.
     """
     text = _text.from_markup(dedent(content).strip())
-    text = text.wrap(
-        console=_console,
-        width=_current_wrap_width() - pad_size,
-        justify=True,
-        tab_size=4,
-    )
-
+    text = text.wrap(console=_console,
+                     width=_current_wrap_width() - _DEFAULT_PAD_SIZE,
+                     justify=True, tab_size=4)
     for line in text:
-        line.pad_left(pad_size)
-
+        line.pad_left(_DEFAULT_PAD_SIZE)
     return text
-
-
-def _remove_leading_underscore(text):
-    """
-    Removes any leading underscores from a given text
-
-    Arguments:
-        text {str}
-
-    Returns:
-        str -- The text without any leading underscores.
-    """
-    return re.sub('^[^A-Za-z]*', '', text)
 
 
 class Help():
     """Help class implementation"""
     # pylint: disable=too-many-arguments
+
     def __init__(
             self,
             name,
@@ -126,104 +97,63 @@ class Help():
             additional_details=None,
     ):
         super().__init__()
-        self._name = name
-        self._brief = brief
-        self._synopsis = synopsis
-        self._description = description
-        self._sub_commands = sub_commands
-        self._options = options
-        self._additional_details = additional_details
+        self.name = name
+        self.brief = brief
+        self.synopsis = synopsis
+        self.description = description
+        self.sub_commands = sub_commands
+        self.options = options
+        self.additional_details = additional_details
+
+        self.heading = name
+        if self.brief:
+            self.heading += ' -- ' + self.brief
 
 
-    def _generate_from_property(self, prop, titlecase=False):
+    def _rich_section(self, name: str, content: str) -> Tuple[Text, Lines]:
         """
-        Uses an underscored property name to generate both a heading for the corresponding
-        help section and format the content for the body of the corresponding help section.
+        Uses an underscored property name to generate both a heading for the
+        corresponding help section and format the content for the body of the
+        corresponding help section.
 
         Arguments:
-            prop {str} -- A property name representing an internal property.
-            Should lead with an underscore (e.g. _name, _description, etc.)
+            name -- The section name.
+            content -- The help content to display for the section.
 
         Returns:
-            tuple(Text, Text) -- a tuple of two rich Text objects, which
-            are formatted to be printed via a rich Console.
+            A tuple of two rich objects:
+                Text - the rich-formatted section heading
+                Lines - the rich-formatted section body
         """
-        name = _remove_leading_underscore(prop).upper()
-        name = name.replace('_', '-')
-
-        if titlecase:
-            name = name.title()
-
+        name = name.upper().replace('_', '-')
         heading = _text.from_markup(_rich_command_heading('\n{}'.format(name)))
-        body = _wrap(getattr(self, prop))
+        body = _wrap_for_console(content)
         return heading, body
 
 
-    def print_help(self):
+    def print_help(self) -> None:
         """Print the help output"""
 
-        # specify the list of attribute values we'd like to show
-        _parts = [
-            '_name',
-            '_synopsis',
-            '_description',
-            '_sub_commands',
-            '_options',
-            '_additional_details',
-        ]
+        parts = list()
 
-        # get a flattened tuple of the corresponding section values
-        # for each of the desirbed parts. Only include sections for which
-        # values have been set.
-        parts = sum(tuple(getattr(self, _remove_leading_underscore(x))
-                          for x in _parts if getattr(self, x)), ())
+        for part in [
+            ('NAME', self.heading),
+            ('SYNOPSIS', self.synopsis),
+            ('DESCRIPTION', self.description),
+            ('Sub-Commands', self.sub_commands),
+            ('OPTIONS', self.options),
+            ('ADDITIONAL DETAILS', self.additional_details),
+        ]:
+            if part[1]:
+                parts.append(self._rich_section(*part))
 
-        # print each of the sections we found.
-        # TODO: Figure out how to capture/supress this, so I can use less for paging the output.
-        _console.print(*parts, sep='\n', end='\n', highlight=False)
-        # output = _console.export_text(clear=False, styles=True)
-        # less(output)
+        parts = sum(tuple(parts), ())
+
+        # Print the paged help info
+        with _console.pager(styles=True):
+            _console.print(*parts, sep='\n', end='\n', highlight=False)
 
         # NOTE: printing an extra blank line at the end here because
         # the 'end' in the console print above is affecting the end of each
         # section as opposed to the end of the text as a whole.
         print('')
-
-
-    def print_usage(self):
-        """Prints the synopsis (usage) of the Help object"""
-        parts = sum(tuple(getattr(self, _remove_leading_underscore(x))
-                          for x in ['_synopsis'] if getattr(self, x)), ())
-        _console.print(*parts, sep='\n', end='\n', highlight=False)
-        print('')
-
-
-    @property
-    def name(self):
-        heading = _text.from_markup(_rich_command_heading('\nNAME'))
-        name_and_brief = '{name}{brief}'.format(
-            name=self._name,
-            brief=' -- %s' % self._brief if self._brief else ''
-        )
-        body = _wrap(name_and_brief)
-        return heading, body
-
-    @property
-    def description(self):
-        return self._generate_from_property('_description')
-
-    @property
-    def synopsis(self):
-        return self._generate_from_property('_synopsis')
-
-    @property
-    def sub_commands(self):
-        return self._generate_from_property('_sub_commands', titlecase=True)
-
-    @property
-    def options(self):
-        return self._generate_from_property('_options')
-
-    @property
-    def additional_details(self):
-        return self._generate_from_property('_additional_details')
